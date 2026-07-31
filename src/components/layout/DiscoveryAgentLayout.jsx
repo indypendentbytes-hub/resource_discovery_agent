1032677a1e97c04d8a303c60c0f03514303e5ecd
Alyssa Duff <indypendentbytes@gmail.com>
2026-07-31 15:44:52 -0400

    Refine Resource Discovery Agent layout

--- src/components/layout/DiscoveryAgentLayout.jsx
+++ src/components/layout/DiscoveryAgentLayout.jsx
@@ -1,14 +1,26 @@
 import ResourcePanel from "../resource/ResourcePanel";
 import ChatPanel from "../chat/ChatPanel";
-import GreenSurface from "../primitives/GreenSurface";
 
-export default function DiscoveryAgentLayout({ messages, resources }) {
+export default function DiscoveryAgentLayout({
+  messages,
+  resources,
+  onSend,
+  onResourceSelect,
+  isSearching,
+}) {
   return (
-    <GreenSurface>
-      <div className="flex flex-col md:flex-row gap-6">
-        <ChatPanel messages={messages} />
-        <ResourcePanel resources={resources} />
+    <div className="overflow-hidden rounded-[2rem] border border-black/10 bg-white shadow-[0_24px_70px_rgba(20,35,25,0.12)] dark:border-white/10 dark:bg-[#181818]">
+      <div className="grid min-h-[520px] lg:grid-cols-[minmax(0,1.55fr)_minmax(300px,0.75fr)]">
+        <ChatPanel
+          messages={messages}
+          onSend={onSend}
+          isSearching={isSearching}
+        />
+        <ResourcePanel
+          resources={resources}
+          onResourceSelect={onResourceSelect}
+        />
       </div>
-    </GreenSurface>
+    </div>
   );
 }
